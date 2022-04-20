874a1305e49e9e9dddb94b26868e11fc27e92bc7
Amr Ehab <amrabdulaziz98@gmail.com>
2022-04-20 04:33:21 +0200

    fixing output + styling
    Co-Authored-By: Amr Ahmed <amr.saber13999@gmail.com>
    Co-Authored-By: Omar Rashed <70315665+OmarRashed-1@users.noreply.github.com>

--- pipeline.py
+++ pipeline.py
@@ -1,8 +1,9 @@
-#imports
+# imports
 import numpy as np
 import cv2
 from moviepy.editor import VideoFileClip
 import sys
+
 
 def abs_sobel_thresh(gray, orient='x', thresh=(0, 255)):
     # Apply x or y gradient with the OpenCV Sobel() function
@@ -20,86 +21,84 @@ def abs_sobel_thresh(gray, orient='x', thresh=(0, 255)):
     # Return the result
     return sobel_mask
 
+
 def dir_threshold(gray, sobel_kernel=3, thresh=(0, np.pi/2)):
     # Grayscale
-    #gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     # Calculate the x and y gradients
     sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
     sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
     # Take the absolute value of the gradient direction, 
     # apply a threshold, and create a binary image result
     absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
-    binary_output =  np.zeros_like(absgraddir)
+    binary_output = np.zeros_like(absgraddir)
     binary_output[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 1
     dir_mask = binary_output
     # Return the binary image
     return dir_mask
 
+
 def combine_thresh(img, s_thresh=(100, 255), l_thresh=(120, 255)):
-    
+
     # Convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     height, width = gray.shape
-    
-    #combined = ((grad_x == 1)  & (grad_y == 1) & (mag_binary == 1) & (dir_binary == 1))
+
+    # combined = ((grad_x == 1)  & (grad_y == 1) & (mag_binary == 1) & (dir_binary == 1))
     grad_x = abs_sobel_thresh(gray, 'x', thresh=(10, 200))
     dir_binary = dir_threshold(gray, thresh=(np.pi/6, np.pi/2))
     combined = ((grad_x == 1) & (dir_binary == 1))
-    
+
     # R & G thresholds so that yellow lanes are detected well.
     color_threshold = 150
-    R = img[:,:,0]
-    G = img[:,:,1]
+    R = img[:, :, 0]
+    G = img[:, :, 1]
     color_combined = np.zeros_like(R)
     r_g_condition = (R > color_threshold) & (G > color_threshold)
-    
+
     # Apply color threshold for better detection of yello and white lines in all environmental condition
     hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
     # Select S channel because it is usually the best performant
     # for this task. R channel also performs similarly.
-    s_channel = hls[:,:,2] 
-    l_channel = hls[:,:,1]
-    
-     # S channel performs well for detecting bright yellow and white lanes
+    s_channel = hls[:, :, 2]
+    l_channel = hls[:, :, 1]
+
+    # S channel performs well for detecting bright yellow and white lanes
     s_condition = (s_channel > s_thresh[0]) & (s_channel <= s_thresh[1])
-    
-    # We put a threshold on the L channel to avoid pixels which have shadows and as a result darker.   
+
+    # We put a threshold on the L channel to avoid pixels which have shadows and as a result darker.
     l_condition = (l_channel > l_thresh[0]) & (l_channel <= l_thresh[1])
-    
+
     # combine all the thresholds
     # A pixel should either be a yellowish or whiteish
     # And it should also have a gradient, as per our thresholds
     color_combined[(r_g_condition & l_condition) & (s_condition | combined)] = 1
-    
-    #binary_output[(s_channel >= thresh[0]) & (s_channel <= thresh[1])] = 1
+
+    # binary_output[(s_channel >= thresh[0]) & (s_channel <= thresh[1])] = 1
     # apply the region of interest mask
     mask = np.zeros_like(color_combined)
-    #np.array([[int(0), int(y)], [int(x), int(y)], [int(0.55*x), int(0.6*y)], [int(0.45*x), int(0.6*y)]]
-    region_of_interest_vertices = np.array([[0,height-1], [width/2, int(0.55*height)], [width-1, height-1]], dtype=np.int32)
-    region_of_disinterest_vertices = np.array([[0.40*width,height-1], [width/2, int(0.55*height)], [width-1-0.40*width, height-1]], dtype=np.int32)
-    #square
-    #region_of_interest_vertices = np.array([[0,height-1],[width-1, height-1], [int(0.55*width), int(0.6*height)],[int(0.45*width), int(0.6*height)] ], dtype=np.int32)
-    #region_of_interest_vertices = np.array([[150,height-1],[1200, height-1], [int(0.55*width), int(0.7*height)],[int(0.45*width), int(0.7*height)] ], dtype=np.int32)
-
+    region_of_interest_vertices = np.array([[0, height-1], [width/2, int(0.55*height)], [width-1, height-1]],
+                                           dtype=np.int32)
     cv2.fillPoly(mask, [region_of_interest_vertices], 1)
-    cv2.fillPoly(mask, [region_of_disinterest_vertices], 0)
-
     thresholded = cv2.bitwise_and(color_combined, mask)
+
     return thresholded
+
 
 def thresholding(img):
 
-    thresholded = combine_thresh(img,s_thresh=(100, 255), l_thresh=(120, 255))
+    thresholded = combine_thresh(img, s_thresh=(100, 255), l_thresh=(120, 255))
     return thresholded
 
-def perspective_warp(img, 
-                     dst_size=(1280,720),
-                     src=np.float32([(0.43,0.65),(0.58,0.65),(0.1,1),(1,1)]),
-                     dst=np.float32([(0,0), (1, 0), (0,1), (1,1)])):
-    img_size = np.float32([(img.shape[1],img.shape[0])])
-    src = src* img_size
+
+def perspective_warp(img,
+                     dst_size=(1280, 720),
+                     src=np.float32([(0.38, 0.7), (0.63, 0.7), (0.1, 1), (1, 1)]),
+                     dst=np.float32([(0, 0), (1, 0), (0, 1), (1, 1)])):
+    img_size = np.float32([(img.shape[1], img.shape[0])])
+    src = src * img_size
     # For destination points, I'm arbitrarily choosing some points to be
-    # a nice fit for displaying our warped result 
+    # a nice fit for displaying our warped result
     # again, not exact, but close enough for our purposes
     dst = dst * np.float32(dst_size)
     # Given src and dst points, calculate the perspective transform matrix
@@ -108,14 +107,15 @@ def perspective_warp(img,
     warped = cv2.warpPerspective(img, M, dst_size)
     return warped
 
-def inv_perspective_warp(img, 
-                     dst_size=(1280,720),
-                     src=np.float32([(0,0), (1, 0), (0,1), (1,1)]),
-                     dst=np.float32([(0.43,0.65),(0.58,0.65),(0.1,1),(1,1)])):
-    img_size = np.float32([(img.shape[1],img.shape[0])])
-    src = src* img_size
+
+def inv_perspective_warp(img,
+                         dst_size=(1280, 720),
+                         src=np.float32([(0, 0), (1, 0), (0, 1), (1, 1)]),
+                         dst=np.float32([(0.38, 0.7), (0.63, 0.7), (0.1, 1), (1, 1)])):
+    img_size = np.float32([(img.shape[1], img.shape[0])])
+    src = src * img_size
     # For destination points, I'm arbitrarily choosing some points to be
-    # a nice fit for displaying our warped result 
+    # a nice fit for displaying our warped result
     # again, not exact, but close enough for our purposes
     dst = dst * np.float32(dst_size)
     # Given src and dst points, calculate the perspective transform matrix
@@ -123,25 +123,26 @@ def inv_perspective_warp(img,
     # Warp the image using OpenCV warpPerspective()
     warped = cv2.warpPerspective(img, M, dst_size)
     return warped
+
 
 def get_hist(img):
     hist = np.sum(img[img.shape[0]//2:,:], axis=0)
     return hist
 
-def sliding_window(img, nwindows=9, margin=150, minpix = 1, draw_windows=True):
-    global left_a, left_b, left_c,right_a, right_b, right_c
-    left_fit_= np.empty(3)
+
+def sliding_window(img, nwindows=9, margin=150, minpix=1, draw_windows=True):
+    global left_a, left_b, left_c, right_a, right_b, right_c
+    left_fit_ = np.empty(3)
     right_fit_ = np.empty(3)
-    
+
     out_img = np.dstack((img, img, img))*255
 
     histogram = get_hist(img)
     # find peaks of left and right halves
-    midpoint = int(histogram.shape[0]/2)
-    leftx_base = np.argmax(histogram[:midpoint])
-    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
-    
-    
+    quarter = int(histogram.shape[0]/4)
+    leftx_base = np.argmax(histogram[:quarter])
+    rightx_base = np.argmax(histogram[quarter*3:]) + quarter*3
+
     # Set height of windows
     window_height = np.int(img.shape[0]/nwindows)
     # Identify the x and y positions of all nonzero pixels in the image
@@ -152,8 +153,7 @@ def sliding_window(img, nwindows=9, margin=150, minpix = 1, draw_windows=True):
     # Current positions to be updated for each window
     leftx_current = leftx_base
     rightx_current = rightx_base
-    
-    
+
     # Create empty lists to receive left and right lane pixel indices
     left_lane_inds = []
     right_lane_inds = []
@@ -168,13 +168,13 @@ def sliding_window(img, nwindows=9, margin=150, minpix = 1, draw_windows=True):
         win_xright_low = rightx_current - margin
         win_xright_high = rightx_current + margin
         # Draw the windows on the visualization image
-        if draw_windows == True:
-            cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),
-            (100,255,255), 3) 
-            cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),
-            (100,255,255), 3) 
+        if draw_windows:
+            cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high),
+            (100, 255, 255), 3)
+            cv2.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high),
+            (100, 255, 255), 3)
         # Identify the nonzero pixels in x and y within the window
-        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
         (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
         good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
         (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
@@ -196,9 +196,9 @@ def sliding_window(img, nwindows=9, margin=150, minpix = 1, draw_windows=True):
     lefty = nonzeroy[left_lane_inds]
     rightx = nonzerox[right_lane_inds]
     righty = nonzeroy[right_lane_inds]
-
+    
     # Fit a second order polynomial to each
-    if(lefty.size >= 3000 and leftx.size >= 3000)or(righty.size >= 3000 and rightx.size >= 3000):
+    if(left_lane_inds.size > 1600) and (right_lane_inds.size > 1600):
         left_fit = np.polyfit(lefty, leftx, 2)
         left_a.append(left_fit[0])
         left_b.append(left_fit[1])
@@ -210,13 +210,14 @@ def sliding_window(img, nwindows=9, margin=150, minpix = 1, draw_windows=True):
         right_c.append(right_fit[2])
 
     else:
-        left_a.append(left_a[-20])
-        left_b.append(left_b[-20])
-        left_c.append(left_c[-20])
-        right_a.append(right_a[-20])
-        right_b.append(right_b[-20])
-        right_c.append(right_c[-20])
+        left_a.append(left_a[-15])
+        left_b.append(left_b[-15])
+        left_c.append(left_c[-15])
+        right_a.append(right_a[-15])
+        right_b.append(right_b[-15])
+        right_c.append(right_c[-15])
 
+    
     left_fit_[0] = np.mean(left_a[-10:])
     left_fit_[1] = np.mean(left_b[-10:])
     left_fit_[2] = np.mean(left_c[-10:])
@@ -225,9 +226,8 @@ def sliding_window(img, nwindows=9, margin=150, minpix = 1, draw_windows=True):
     right_fit_[1] = np.mean(right_b[-10:])
     right_fit_[2] = np.mean(right_c[-10:])
     
-    
     # Generate x and y values for plotting
-    ploty = np.linspace(0, img.shape[0]-1, img.shape[0] )
+    ploty = np.linspace(0, img.shape[0]-1, img.shape[0])
     left_fitx = left_fit_[0]*ploty**2 + left_fit_[1]*ploty + left_fit_[2]
     right_fitx = right_fit_[0]*ploty**2 + right_fit_[1]*ploty + right_fit_[2]
 
@@ -235,6 +235,7 @@ def sliding_window(img, nwindows=9, margin=150, minpix = 1, draw_windows=True):
     out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 100, 255]
     
     return out_img, (left_fitx, right_fitx), (left_fit_, right_fit_), ploty
+
 
 def get_curve(img, leftx, rightx):
     ploty = np.linspace(0, img.shape[0]-1, img.shape[0])
@@ -259,6 +260,7 @@ def get_curve(img, leftx, rightx):
     # Now our radius of curvature is in meters
     return (left_curverad, right_curverad, center)
 
+
 def draw_lanes(img, left_fit, right_fit):
     ploty = np.linspace(0, img.shape[0]-1, img.shape[0])
     color_img = np.zeros_like(img)
@@ -272,10 +274,9 @@ def draw_lanes(img, left_fit, right_fit):
     inv_perspective = cv2.addWeighted(img, 1, inv_perspective, 0.7, 0)
     return inv_perspective
 
+
 def full_pipeline(img):
-#     undist_img = undistort(img)
-    undist_img = img
-    th_img = thresholding(undist_img)
+    th_img = thresholding(img)
 
     pew_img = perspective_warp(th_img)
 
@@ -302,13 +303,12 @@ def full_pipeline(img):
     
     return final_img
 
-if __name__ == '__main__':
-    right_curves, left_curves = [],[]
-    left_a, left_b, left_c = [],[],[]
-    right_a, right_b, right_c = [],[],[]
 
-    myclip = VideoFileClip(sys.argv[1]).subclip(0,10)
+if __name__ == '__main__':
+    right_curves, left_curves = [], []
+    left_a, left_b, left_c = [], [], []
+    right_a, right_b, right_c = [], [], []
+    myclip = VideoFileClip(sys.argv[1])
     output_vid = sys.argv[2]
     clip = myclip.fl_image(full_pipeline)
-
     clip.write_videofile(output_vid, audio=False)

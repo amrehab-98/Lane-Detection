d4eae2da46d2690ecdb2d383234c346e4c171bf6
AmrAhmed11 <57914179+AmrAhmed11@users.noreply.github.com>
2022-05-23 18:56:05 +0200

    phase 2 batch file

--- Phase 2/phase2.bat
+++ Phase 2/phase2.bat
@@ -1,32 +1,65 @@
 @echo off
+setlocal ENABLEDELAYEDEXPANSION
 
 IF "%1" == "" (
     echo "error1"
+    exit 1
 )
 
 IF "%2" == "" (
-    SET var1="%~dp0output.mp4"
-    SET var2= "0"
-
-    python phase2.py %1 %var1% %var2%
+    SET outPath="%~dp0output.mp4"
+    SET debugMode="0"
+    SET phase="0"
+    python phase2.py %1 !outPath! !debugMode! !phase!
+    exit 0
 )
 
 IF "%2" == "--debug" (
-    SET var1="%~dp0output.mp4"
-    SET var2= %3
-
-    python phase2.py %1 %var1% %var2%
+    SET outPath="%~dp0output.mp4"
+    SET debugMode="%3"
+    IF "%4" == "--phase" (
+        SET phase="%5"
+    ) ELSE (
+        SET phase="0"
+    )
+    python phase2.py %1 !outPath! !debugMode! !phase!
+    exit 0
 )
 
-IF NOT "%2" == "" (
-    IF "%3" == "" (
-        SET var2= "0"
-        python phase2.py %1 %2 %var2%
-    )
-    IF "%3" == "--debug" (
-        IF NOT "%4" == "" (
-            python phase2.py %1 %2 %4
-        )
-    )
+IF "%2" == "--phase" (
+    SET outPath="%~dp0output.mp4"
+    SET debugMode="0"
+    SET phase="%3"
+    python phase2.py %1 !outPath! !debugMode! !phase!
+    exit 0
 )
+
+IF "%3" == "" (
+    SET outPath="%2"
+    SET debugMode="0"
+    SET phase="0"
+    python phase2.py %1 !outPath! !debugMode! !phase!
+    exit 0
+)
+
+IF "%3" == "--debug" (
+    SET outPath="%2"
+    SET debugMode="%4"
+    IF "%5" == "--phase" (
+        SET phase="%6"
+    ) ELSE (
+        SET phase="0"
+    )
+    python phase2.py %1 !outPath! !debugMode! !phase!
+    exit 0
+)
+
+IF "%3" == "--phase" (
+    SET outPath="%2"
+    SET debugMode="0"
+    SET phase=%4
+    python phase2.py %1 !outPath! !debugMode! !phase!
+    exit 0
+)
+
 pause
